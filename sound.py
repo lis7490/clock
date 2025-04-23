b6657fe5dd25a0ca7c0058d00dc5d78942a5aa2d
Stupnikov Dmitry <d_lis82@mail.ru>
2025-04-23 16:54:26 +0300

    изменения в sound 2

--- sound.py
+++ sound.py
@@ -4,7 +4,8 @@ import time as time_module
 a = []
 b = []
 dict = ""
-
+e = 0
+f = 0
 while dict != "нет":
     user_input = input("Введите время в формате Часы:Минуты : ")
     c, d = map(int, user_input.split(':'))
@@ -15,15 +16,17 @@ while dict != "нет":
 while True:
     current_time = datetime.now().time()  # Текущее время
     print(f"\rТекущее время: {current_time.strftime('%H:%M:%S')}", end="", flush=True)
-
+    e = 0
+    f = 0
     for i in a:
         if current_time.hour == i:
-            d = 1
+            e = 1
     for j in b:
         if current_time.minute == j:
-            c = 1
-    if d == 1 and c == 1:
-        print("ДААААА")
+            f = 1
+    if e == 1 and f == 1:
+        print(f"\nЗвонит БУДИЛЬНИК! Дзынь Дзынь! Просыпайся, лодырь, пора пахать!)")
+        time_module.sleep(60)
     time_module.sleep(1)
 
 
